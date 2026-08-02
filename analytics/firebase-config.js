51e0396de5547cc6045c7db4865a5fc0945fd830
Traqnivanov <81027881+Traqnivanov@users.noreply.github.com>
2026-08-02 07:05:33 +0300

    Delete analytics directory

--- analytics/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDo64tkV1actsi48k15KMfo8Be3opXSQ80",
-  authDomain: "ivanov-analytics.firebaseapp.com",
-  projectId: "ivanov-analytics",
-  storageBucket: "ivanov-analytics.firebasestorage.app",
-  messagingSenderId: "42662480154",
-  appId: "1:42662480154:web:7ac298adcb2892677b84a3"
-};
-export const OWNER_UID = "uqAZqxwN1wTQSlo2HODMmvHv1nj2";
